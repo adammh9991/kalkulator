fc28bea3869c295311eef09f16ddb3f186eb4585
adammh9991 <adam.tajny98@gmail.com>
2020-11-14 14:42:30 +0100

    Szablonowanie

--- jsf_01/src/com/jsfcourse/calc/KalkulatorBB.java
+++ jsf_01/src/com/jsfcourse/calc/KalkulatorBB.java
@@ -3,7 +3,6 @@ package com.jsfcourse.calc;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
